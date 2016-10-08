1e36cfa4381192d576164d4511af7945a48e09dc
pitercito <pitercito1990@gmail.com>
2016-10-08 14:49:52 +0200

    Cube with velocity

---
+++ Assets/Flappy/flappyScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flappyScript : MonoBehaviour {
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fly ();
+	
+	}
+
+	void Fly(){
+		if (Input.GetKeyDown ("space")) {
+			print ("fly");
+			//transform.Translate(Vector3.up * Time.deltaTime*10, Space.World);
+			rb.velocity= new Vector3(0,5,0);
+
+		}
+	}
+}
